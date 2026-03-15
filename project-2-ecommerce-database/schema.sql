6ff0346cc89bd3b49f6ec0e56da2d19da2429c4a
Aman Bhaskar <amanbhaskarcodes@gmail.com>
2026-03-15 20:09:34 +0530

    Add initial database schema for e-commerce project, including tables for categories, users, sellers, and products with appropriate foreign key constraints.

---
+++ project-2-ecommerce-database/schema.sql
@@ -0,0 +1,48 @@
+CREATE TABLE categories (
+    category_id SERIAL PRIMARY KEY,
+    category_name TEXT NOT NULL,
+    parent_category_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_parent_category
+        FOREIGN KEY(parent_category_id)
+        REFERENCES categories(category_id)
+);
+
+
+CREATE TABLE users (
+    user_id BIGSERIAL PRIMARY KEY,
+    full_name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    phone TEXT UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    is_active BOOLEAN DEFAULT TRUE
+);
+
+
+CREATE TABLE sellers (
+    seller_id BIGSERIAL PRIMARY KEY,
+    seller_name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    rating NUMERIC(3,2) DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE products (
+    product_id BIGSERIAL PRIMARY KEY,
+    seller_id BIGINT NOT NULL,
+    category_id INT NOT NULL,
+    product_name TEXT NOT NULL,
+    price NUMERIC(10,2) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_seller
+        FOREIGN KEY (seller_id)
+        REFERENCES sellers(seller_id),
+
+    CONSTRAINT fk_category
+        FOREIGN KEY (category_id)
+        REFERENCES categories(category_id)
+);
